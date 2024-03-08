23c02ca71a4da459d7ac8128cd4c7d3ba8973052
suraj.pinjan <suraj@zogato.com>
2024-03-08 10:38:42 +0530

    update

--- src/components/Project/PDFGen.tsx
+++ src/components/Project/PDFGen.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@mui/material";
 import React from "react";
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+// import pdfFonts from "pdfmake/build/vfs_fonts";
 import {
   _JSONToPDFtransformer,
   jsonToMap,
 } from "../../services/JSONToPDFTransformer";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 import { TDocumentDefinitions } from "pdfmake/interfaces";
 import { urlToBase64 } from "../../services/ApiService";
